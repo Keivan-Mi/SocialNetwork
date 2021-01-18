4372c5fce602c79f4c1e2320cb3ab1280314a782
ashkanmsz <ashkanmsz7878@gmail.com>
2021-01-18 20:52:11 +0330

    post view was created

--- storage/framework/views/f5d5eedca00d84e4ec61e258a9afbe81aee16e0b.php
+++ storage/framework/views/f5d5eedca00d84e4ec61e258a9afbe81aee16e0b.php
@@ -6,13 +6,33 @@
         </div>
         <div class="col-4">
             <div>
-                <h3><?php echo e($post->user->username); ?></h3>
+                <div class="d-flex align-items-center">
+                    <div class="pr-3">
+                        <img src="/storage/<?php echo e($post->user->profile->image); ?>" class="rounded-circle w-100" style="max-width: 40px;">
+                    </div>
+                    <div>
+                        <div class="font-weight-bold">
+                            <a href="/profile/<?php echo e($post->user->id); ?>">
+                                <span class="text-dark"><?php echo e($post->user->username); ?></span>
+                            </a>
+                            <a href="#" class="pl-3">Follow</a>
+                        </div>
+                    </div>
+                </div>
 
-                <p><?php echo e($post->caption); ?></p>
+                <hr>
+
+                <p>
+                    <span class="font-weight-bold">
+                        <a href="/profile/<?php echo e($post->user->id); ?>">
+                            <span class="text-dark"><?php echo e($post->user->username); ?></span>
+                        </a>
+                    </span> <?php echo e($post->caption); ?>
+
+                </p>
             </div>
         </div>
     </div>
 </div>
 <?php $__env->stopSection(); ?>
-
 <?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Saeed.R\Desktop\SocialNetwork\resources\views/posts/show.blade.php ENDPATH**/ ?>
