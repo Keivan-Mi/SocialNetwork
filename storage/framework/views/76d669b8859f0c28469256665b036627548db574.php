5cab8de88d5b15e84f17f834761cc158d31a9278
S.Hossein Mirhosseini <keivan15@ymail.com>
2021-01-25 13:31:50 +0330

    Update the readme file

---
+++ storage/framework/views/76d669b8859f0c28469256665b036627548db574.php
@@ -0,0 +1,63 @@
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row">
+        <!--Profile image -->
+        <div class="d-flex">
+            <div class="col-3 p-5">
+                <img src="<?php echo e($user->profile->profileImage()); ?>" class="w-100 rounded-circle" alt="Not found!!!">
+            </div>
+
+            <!--Profile info section _ using View.js (post + followers + following) -->
+            <div class="row d-inline-block">
+                <follow-button user-id="<?php echo e($user->id); ?>" follow="<?php echo e($follows); ?>" username="<?php echo e($user->username); ?>" posts="<?php echo e($user->posts->count()); ?>" followers="<?php echo e($user->profile->followers->count()); ?>" following="<?php echo e($user->following->count()); ?>" ownprofile="<?php echo e($ownProfile); ?>">
+                </follow-button>
+
+                <!--Profile description section -->
+                <div class="pt-2">
+                    <div class="font-weight-bold "><?php echo e($user->profile->title); ?></div>
+                    <div> <?php echo e($user->profile->description); ?> </div>
+                    <div><a target="_blank" href="<?php echo e($user->profile->url); ?>" class="font-weight-light"> <?php echo e($user->profile->url); ?> </a></div>
+                </div>
+            </div>
+
+            <!--Edit profile and Add post section -->
+            <div class="m-auto">
+                <?php if (app(\Illuminate\Contracts\Auth\Access\Gate::class)->check('update',$user->profile)): ?>
+                <a href="/profile/<?php echo e($user->id); ?>/edit" class="btn btn-light mr-1" style="background-color:white; border:none" data-toggle="tooltip" data-placement="top" title="Tooltip on top"><img src="https://img.icons8.com/material-rounded/24/000000/settings.png" /></a>
+                <a href="/" class="btn btn-light mr-1" style="background-color:white; border:none"><img src="https://img.icons8.com/fluent-systems-filled/24/000000/home.png" /></a>
+                <a href="/p/create" class="btn btn-light mr-1" style="background-color:white; border:none"><img src="https://img.icons8.com/metro/26/000000/plus-2-math.png" /></a>
+                <form action="<?php echo e(route('profile.delete', $user->id)); ?>" method="post">
+                    <?php echo csrf_field(); ?>
+                    <button type="submit" class="btn btn-light mr-5" data-toggle="tooltip" data-placement="left" title="Delete Account" style="background-color:white; border:none">
+                        <img src="https://img.icons8.com/ios-glyphs/30/000000/remove-user-male.png" />
+                    </button>
+                </form>
+
+                <?php endif; ?>
+            </div>
+        </div>
+
+        <!--Show posts (at first, just using 3 images  -->
+        <div class="container row pt-4 border-top mt-3">
+            <!-- show all posts of this user from DB -->
+            <?php $__currentLoopData = $user->posts; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $post): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+            <div class="col-4">
+                <div class=" p-2 mb-4 border">
+                    <a href="/p/<?php echo e($post->id); ?>">
+                        <img src="/storage/<?php echo e($post->image); ?>" class="w-100" alt="not found">
+                    </a>
+                    <div class="mt-2">
+                        <?php if (app(\Illuminate\Contracts\Auth\Access\Gate::class)->check('delete', $user->profile)): ?>
+                        <a href="<?php echo e(route('post.delete',['post_id' => $post->id ])); ?>">
+                            <img src="https://img.icons8.com/fluent-systems-regular/28/000000/delete-trash--v3.png" />
+                        </a>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </div>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH E:\University\Term 7\Internet Engineering\Project\Final\SocialNetwork\resources\views/profiles/index.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/bb8bc3bffb76990c84ed82dabbc5154d84f0e982.php
@@ -0,0 +1,66 @@
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <form action="/profile/<?php echo e($user->id); ?>" enctype="multipart/form-data" method="post">
+        <?php echo csrf_field(); ?>
+        <?php echo method_field('PATCH'); ?>
+
+        <div class="row">
+            <div class="col-8 offset-2">
+
+                <div class="form-group row">
+                    <label for="title" class="col-md-4 col-form-label">Title</label>
+
+                    <input id="title" type="text" class="ml-2 form-control<?php echo e($errors->has('title') ? 'is-invalid' : ''); ?>" name="title" value="<?php echo e(old('title') ?? $user->profile->title); ?>" autocomplete="title" autofocus>
+
+                    <?php if($errors->has('title')): ?>
+                    <span class="invalid-feedback" role="alert">
+                        <strong><?php echo e($errors->first('title')); ?></strong>
+                    </span>
+                    <?php endif; ?>
+                </div>
+
+                <div class="form-group row">
+                    <label for="description" class="col-md-4 col-form-label">Description</label>
+
+                    <input id="description" type="text" class="ml-2 form-control<?php echo e($errors->has('description') ? ' is-invalid' : ''); ?>" name="description" value="<?php echo e(old('description') ?? $user->profile->description); ?>" autocomplete="description" autofocus>
+
+                    <?php if($errors->has('description')): ?>
+                    <span class="invalid-feedback" role="alert">
+                        <strong><?php echo e($errors->first('description')); ?></strong>
+                    </span>
+                    <?php endif; ?>
+                </div>
+
+                <div class="form-group row">
+                    <label for="url" class="col-md-4 col-form-label">URL</label>
+
+                    <input id="url" type="text" class=" ml-2 form-control<?php echo e($errors->has('url') ? ' is-invalid' : ''); ?>" name="url" value="<?php echo e(old('url') ?? $user->profile->url); ?>" autocomplete="url" autofocus>
+
+                    <?php if($errors->has('url')): ?>
+                    <span class="invalid-feedback" role="alert">
+                        <strong><?php echo e($errors->first('url')); ?></strong>
+                    </span>
+                    <?php endif; ?>
+                </div>
+
+                <div class="row">
+                    <label for="image" class="col-md-4 col-form-label">Profile Image</label>
+
+                    <input type="file" class=" ml-2 form-control-file" id="image" name="image">
+
+                    <?php if($errors->has('image')): ?>
+                    <strong><?php echo e($errors->first('image')); ?></strong>
+                    <?php endif; ?>
+                </div>
+
+                <div class="row pt-4">
+                <button class=" ml-2 btn btn-primary" style="background-color:black; border: black">Save</button>
+                </div>
+
+            </div>
+        </div>
+    </form>
+
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH E:\University\Term 7\Internet Engineering\Project\Final\SocialNetwork\resources\views/profiles/edit.blade.php ENDPATH**/ ?>
